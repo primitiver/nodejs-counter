e48ae6ae94dbff2dbe62ec04bae3ed17e8776d11
email <517295368@qq.com>
2020-08-06 17:40:04 +0800

    fix

--- model/db.js
+++ model/db.js
@@ -10,8 +10,8 @@ var options = {
 };
 
 // var dbURL = "mongodb://" + options.db_user + ":" + options.db_pwd + "@" + options.db_host + ":" + options.db_port + "/" + options.db_name;
-// var dbURL = "mongodb://60.205.0.3:27017/derucci-consumer";
-var dbURL = "mongodb://127.0.0.1:27017/derucci-consumer";
+var dbURL = "mongodb://60.205.0.3:27017/derucci-consumer";
+// var dbURL = "mongodb://127.0.0.1:27017/derucci-consumer";
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }); // 连接数据库
 
 mongoose.connection.on("connected", () => {
